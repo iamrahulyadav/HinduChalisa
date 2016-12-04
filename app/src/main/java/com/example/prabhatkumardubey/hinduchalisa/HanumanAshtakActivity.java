59ec54d49ca4e29cd42556ba5ac306578b948f10
Prabhat kumar Dubey <prabhatdubeytheking@gmail.com>
2016-12-04 19:35:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prabhatkumardubey/hinduchalisa/HanumanAshtakActivity.java
@@ -0,0 +1,22 @@
+package com.example.prabhatkumardubey.hinduchalisa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by Prabhat kumar Dubey on 04-12-2016.
+ */
+
+public class HanumanAshtakActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_hanumanashtak);
+
+    }
+}
